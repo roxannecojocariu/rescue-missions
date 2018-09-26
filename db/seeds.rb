e5eddef33c3a39625728695239087695c89df38e
Roxanne Cojocariu <rmcojocariu@gmail.com>
2018-09-26 19:22:54 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user1 = User.create(email:"cooldude@launchacademy.com", password: "gregsucks")
-user2 = User.create(email:"yesman@launchacademy.com", password: "gregsucks")
-user3 = User.create(email:"whatsup@launchacademy.com", password: "gregsucks")
+user1 = User.create(email:"cooldude@cooldude.com", password: "123456")
+user2 = User.create(email:"yesman@yesman.com", password: "123456")
+user3 = User.create(email:"whatsup@whatsup.com", password: "123456")
 
 question_data = [
   {
-    title: "Why can't I figure out faker?",
-    description: "I would love to be able to not have to hand write all of these questions, but I don't know how to fix this.",
+    title: "How much wood could a #{Faker::Pokemon.name} chuck if a #{Faker::Pokemon.name} could chuck wood?",
+    description: "#{Faker::DrWho.quote} #{Faker::HarryPotter.spell} #{Faker::HarryPotter.spell} #{Faker::HarryPotter.spell}",
     user: user1
   },
   {
-    title: "How much would I get if I give someone my liver?",
-    description: "I'm really broke and looking for a way to make some extra cash, but I don't want anyone to judge me.  Can someone please help me?",
+    title: "How much wood could a #{Faker::Pokemon.name} chuck if a #{Faker::Pokemon.name} could chuck wood?",
+    description: "#{Faker::DrWho.quote} #{Faker::HarryPotter.spell} #{Faker::HarryPotter.spell} #{Faker::HarryPotter.spell}",
     user: user2
   },
   {
-    title: "What is this thing called life?",
-    description: "I'm not sure that I am living a very fulfilled life and hope that I can change this so I'm living life to the fullest.  Plz help.",
+    title: "How much wood could a #{Faker::Pokemon.name} chuck if a #{Faker::Pokemon.name} could chuck wood?",
+    description: "#{Faker::DrWho.quote} #{Faker::HarryPotter.spell} #{Faker::HarryPotter.spell} #{Faker::HarryPotter.spell}",
     user: user3
   }
 ]
@@ -33,22 +33,22 @@ end
 
 answer_data = [
   {
-    description: "If you want the answer to this question, you will have to spin around 10 times and then cluck like a chicken.",
+    description: "#{Faker::DrWho.quote}.  If you're still stuck, you should take a look at some useful links about #{Faker::Pokemon.name} for help.",
     user: user1,
     question: Question.first
   },
   {
-    description: "Hopefully this works because I can safely say that I KIND OF know what's going on, but as always, I'm not quite there.",
+    description: "#{Faker::DrWho.quote}.  If you're still stuck, you should take a look at some useful links about #{Faker::Pokemon.name} for help.",
     user: user1,
     question: Question.second
   },
   {
-    description: "I'm trying to figure this out the best that I can.  Hopefully this works because I can safely say that I KIND OF know what's going on, but as always, I'm not quite there.",
+    description: "#{Faker::DrWho.quote}.  If you're still stuck, you should take a look at some useful links about #{Faker::Pokemon.name} for help.",
     user: user2,
     question: Question.first
   },
   {
-    description: "Let's see what happens here.  I'm trying to figure this out the best that I can.  Hopefully this works because I can safely say that I KIND OF know what's going on, but as always, I'm not quite there.",
+    description: "#{Faker::DrWho.quote}.  If you're still stuck, you should take a look at some useful links about #{Faker::Pokemon.name} for help.",
     user: user2,
     question: Question.third
   }
